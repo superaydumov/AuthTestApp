7697905196fa47497aebc850bc47447c826b0a82
Eldar Aidumov <superaydumov@me.com>
2025-09-04 16:35:52 +0300

    Add files via upload

---
+++ AuthTestApp/AuthTestApp/AuthTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthTestAppApp.swift
+//  AuthTestApp
+//
+//  Created by Эльдар Айдумов on 04.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct AuthTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
